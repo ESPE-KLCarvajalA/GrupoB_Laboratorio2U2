158322160f2f15257ac3842269032315992d3691
ESPE-KLCarvajalA <135269362+ESPE-KLCarvajalA@users.noreply.github.com>
2024-07-22 02:52:55 -0500

    About 2

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import {  EventEmitter } from '@angular/core';
 import {  Output } from '@angular/core';
+import {  HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-about',
@@ -12,7 +13,21 @@ import {  Output } from '@angular/core';
 export class AboutComponent {
   @Output() showBio = new EventEmitter<void>();
 
-  emitShowBio() {
-    this.showBio.emit();
+  onShowBioClick() {
+    this.showBio.emit(); // Emitir el evento para el componente padre
   }
+
+  @HostListener('click', ['$event'])
+  onClick(event: MouseEvent) {
+    alert('Mostrando más datos');
+  }
+
+  
+  titulo="Sobre Nosotros"
+  cargo='Fotógrafo Profesional'
+  imagenUrl= '../../img.jpg';
+
+    paragraph1: string = '¡Hola! Soy Emily Johnson, una fotógrafa profesional con sede en la vibrante ciudad de Nueva York. Con una pasión por capturar momentos auténticos y emociones genuinas, me especializo en retratos, fotografía de bodas y fotografía de estilo de vida.';
+    paragraph2: string = 'No puedo esperar para conectarme contigo y ayudarte a capturar momentos que perdurarán para siempre. Ya sea que estés planeando una sesión de fotos íntima o una boda extravagante, ¡estoy aquí para hacer realidad tus visiones fotográficas!';
+
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,7 +21,7 @@ import { CommonModule } from '@angular/common';
 })
 export class AppComponent {
   bioVisible = false;
-  bio = 'Emily Johnson is a professional photographer with over 10 years of experience in capturing the beauty of life through her lens.';
+  bio = 'Mi trayectoria como fotógrafa profesional se ha definido por mi dedicación a capturar la esencia de cada momento. Desde las risas espontáneas hasta los gestos más sutiles, mi objetivo es inmortalizar la belleza y la autenticidad en cada imagen que creo. Con una formación académica sólida y años de experiencia en el campo, he perfeccionado mi arte para ofrecer a mis clientes resultados excepcionales. ';
 
   handleShowBio() {
     this.bioVisible = !this.bioVisible; // Toggle the bio visibility
